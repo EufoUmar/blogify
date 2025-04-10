55fd21608ce811573d012109b41e7e5f85a22e9c
umar farooq <umar.eufo@gmail.com>
2025-04-10 12:34:13 +0500

    salt made

--- index.js
+++ index.js
@@ -1,33 +1,17 @@
-const express = require("express")
-const path = require("path")
-const userRoute = require("./routes/user")
-const mongoose = require("mongoose")
-const cookieParser = require("cookie-parser");
-const { checkForAuthenticationCokie } = require("./middleware/authentication");
+const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = 8000;
 
-app.set("view engin", "ejs")
+app.set("view engine",  "ejs");
 app.set("views", path.resolve("./views"))
-app.engine('html', require('ejs').renderFile); // Example for EJS
-app.set('views', path.join(__dirname, 'views')); // Set the views directory
-
- 
-// connection mongoDB
-mongoose.connect("mongodb://127.0.0.1:27017/blogify").then((e) => console.log("MongoDB is connected"))
-
-
-// middleWare
-app.use(express.urlencoded({ extended: false}))
-app.use(cookieParser());
-app.use(checkForAuthenticationCokie("token"));
 
 app.get("/", (req, res) => {
-     res.render("home.ejs", {
-         user: req.user,
-     });
-})
-app.use("/user", userRoute)
+    res.render("home")
+});
 
-app.listen(PORT, () => console.log(`Server Started At Port ${PORT}`))
+app.listen(PORT, () => {
+    console.log(`Server started at PORT ${PORT}`);
+})
+
--- models/user.js
+++ models/user.js
@@ -1,71 +1,43 @@
-const { createHmac, randomBytes } = require("crypto")
-const { Schema, model } = require("mongoose")
-const {createTokenForUser} = require("../services/authentication")
+const {Schema, model} = require("mongoose");
+const {createHmac, randomBytes} = require("crypto");
 
 const userSchema = new Schema({
-   fullName: {
-    type: String,
-    required: true
-   },
-   email: {
-    type: String,
-    required: true,
-    unique: true
-   },
-   password: {
-    type: String, 
-    required: true
-   },
-   salt: {
-    type: String,
-    // required: true
-   },
-   password: {
-    type: String,
-    required: true
-   },
-   profileImage: {
-    type: String,
-    default: "/images/default.png"
-   },
-   role: {
-    type: String,
-    enum: ["USER", "ADMIN"],
-    default: "USER"
-   }
-}, { timestamps: true} )  
+    FullName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    salt: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    profileImageURL: {
+        type: String,
+        default: "/images/default.jpg",
+    },
+    role: {
+        type: String,
+        enum: ["USER", "ADMIN"],
+        default: "USER",
+    }
+})
+userSchema.pre("save", function(next) {
+    const user = this;
+    if (!user.isModified("password")) return;
 
-userSchema.pre("save", function (next) {
-    const user = this;    
-
-    if (!user.isModified("password")) return;  
-
-    const salt = randomBytes(16).toString();
+    const salt = randomBtytes(16).toString()
     const hashedPassword = createHmac("sha256", salt).update(user.password).digest("hex")
-
-     this.salt = salt;
-     this.password = hashedPassword
+    this.salt = salt;
+    this.password = hashedPassword;
 
     next();
 })
-
-userSchema.static("matchPasswordGenerateToken", async function(email, password){
-    const user = await this.findOne({ email });
-    
-    if(!user) throw new Error("User not found");
-        const salt = user.salt
-        const hashedPassword = user.password;
-        const userProvidedHash = createHmac('sha256', salt)
-        .update(password)
-        .digest("hex");
-    
-        if(hashedPassword !== userProvidedHash) throw new Error("Incorrect Password")
-    
-        const token = createTokenForUser(user)
-        return token;
-})
-
 const User = model("user", userSchema);
-
-module.exports = User;
-  
